afc6e63379e047c78289dea43e98f042d409e88c
Ervin Ademovic <newea1984@gmail.com>
2020-05-27 09:37:53 +0200

    Exercise One

---
+++ Basic/Exercise 1/script.js
@@ -0,0 +1,9 @@
+function crytalGazer(children, partner, geo, job){
+    let val = `You will be a ${job} in ${geo} and married to ${partner} with ${children} childrens.`;
+    return val;
+}
+
+let result = crytalGazer(2, "Marie", "Vienna", "Programmer");
+
+console.log(result);
+
